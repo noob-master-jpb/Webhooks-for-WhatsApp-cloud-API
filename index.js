9e55aa8415e580f8fb6d27a6e87a5121281dba72
noob-master-jpb/ <b8777051595@gmail.com>
2025-08-10 15:05:40 +0530

    .

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.listen(process.env.PORT,()=>{
 //to verify the callback url from dashboard side - cloud api side
 app.get("/webhook",(req,res)=>{
    let mode=req.query["hub.mode"];
-   let challange=req.query["hub.challenge"];
+   let challenge=req.query["hub.challenge"];
    let token=req.query["hub.verify_token"];
 
 
     if(mode && token){
 
         if(mode==="subscribe" && token===mytoken){
-            res.status(200).send(challange);
+            res.status(200).send(challenge);
         }else{
             res.status(403);
         }
@@ -46,30 +46,47 @@ app.post("/webhook",(req,res)=>{ //i want some
             ){
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+               let message = body_param.entry[0].changes[0].value.messages[0];
+               
+               // Check if it's a text message
+               if (message.type === 'text') {
+                   let msg_body = message.text.body;
 
-               console.log("phone number "+phon_no_id);
-               console.log("from "+from);
-               console.log("boady param "+msg_body);
+                   console.log("phone number "+phon_no_id);
+                   console.log("from "+from);
+                   console.log("body param "+msg_body);
 
-               axios({
-                   method:"POST",
-                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
-                   data:{
-                       messaging_product:"whatsapp",
-                       to:from,
-                       text:{
-                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                   axios({
+                       method:"POST",
+                       url:"https://graph.facebook.com/v21.0/"+phon_no_id+"/messages?access_token="+token,
+                       data:{
+                           messaging_product:"whatsapp",
+                           to:from,
+                           text:{
+                               body:"Hi.. I'm Prasath, your message is "+msg_body
+                           }
+                       },
+                       headers:{
+                           "Content-Type":"application/json"
                        }
-                   },
-                   headers:{
-                       "Content-Type":"application/json"
-                   }
 
-               });
+                   }).then(response => {
+                       console.log("Message sent successfully");
+                   }).catch(error => {
+                       console.error("Error sending message:", error.response?.data || error.message);
+                   });
+               } else {
+                   console.log("Received non-text message:", message.type);
+               }
 
                res.sendStatus(200);
-            }else{
+            } else if (body_param.entry && 
+                       body_param.entry[0].changes && 
+                       body_param.entry[0].changes[0].value.statuses) {
+                // Handle message status updates (delivered, read, etc.)
+                console.log("Received message status update");
+                res.sendStatus(200);
+            } else {
                 res.sendStatus(404);
             }
 
